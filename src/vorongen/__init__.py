3d7b93efc16495ffe89ac2c600b69b05d6209eee
temur-mn <timamsv5@gmail.com>
2026-02-10 11:44:23 +0000

    few_structural_adjustments

--- src/vorongen/__init__.py
+++ src/vorongen/__init__.py
@@ -1,3 +1,26 @@
-"""
-Synthetic data generator package (binary generator with extension hooks).
-"""
+"""Voronogeen package: import-first API over the vorongen core engine."""
+
+from .pipeline import (
+    VorongenSynthesizer,
+    compare_torch_vs_classic,
+    generate,
+    load_config,
+)
+from .samples import available_sample_configs, get_sample_config, get_sample_yaml
+from .settings import RunConfig, SynthesisResult, TorchControllerConfig
+from .torch_controller import TorchPenaltyController, is_torch_available
+
+__all__ = [
+    "RunConfig",
+    "SynthesisResult",
+    "TorchControllerConfig",
+    "TorchPenaltyController",
+    "VoronogeenSynthesizer",
+    "available_sample_configs",
+    "compare_torch_vs_classic",
+    "generate",
+    "get_sample_config",
+    "get_sample_yaml",
+    "is_torch_available",
+    "load_config",
+]
